4065ff1d551ba1b90697e8aab684bfe16072b752
Elkin Oved Ortiz Salazar <eortizs@ucentral.edu.co>
2020-09-06 11:01:26 -0500

    Operación Suma

---
+++ WS Calculadora/Calculadora/Controllers/SumaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace Calculadora.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SumaController : ControllerBase
+    {
+        //Controlador encargado de realizar la suma
+        [HttpPost]
+        public async Task<Int32> Post(int OperandoUno, int OperandoDos) {
+            var Resultado = OperandoUno + OperandoDos;
+            return Resultado;
+        }
+    }
+}
